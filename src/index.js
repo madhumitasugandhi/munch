ae2b427b0088442af81d6bc64142474794209dae
madhumitasugandhi <madhumitasugandhi@gmail.com>
2024-11-12 15:32:08 +0530

    React first appication

--- src/index.js
+++ src/index.js
@@ -2,4 +2,21 @@ import { createRoot } from "react-dom/client";
 
 const root = createRoot(document.getElementById("root"));
 
-root.render(<h1>Munch</h1>)
+function Chocolate(props) {
+
+    const {name ,City} = props;
+
+    return (<>
+        <h2>{name} - {City}</h2>
+        <hr/>
+    </>)
+}
+
+root.render(<>
+    <h1>All Chocolate</h1>
+
+    <Chocolate name="Madhumita" City="Patur"/>
+    <Chocolate name="pooja" City="Washim"/>
+    <Chocolate name="Rishi" City="Akot"/>
+    
+</>)
